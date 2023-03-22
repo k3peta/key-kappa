b2e5167d9b08c54625f9998f82d91d75a63a7f92
k3peta <k3peta@gmail.com>
2023-03-22 20:52:02 +0900

    QMK Firmware関係

---
+++ kappa/kappa.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "quantum.h"
+
+#define XXX KC_NO
+
+#define LAYOUT( \
+	k0A, k3A, k1B, k4B, k2C, k0D, k3D, k1E, k4E, k2F, k1A, k4A, k2B, k0C, k3C, k1D, k4D, k2E, k0F, k3F, k2A, k0B, k3B, k1C, k4C, k2D, k0E, k3E, k1F, k4F \
+) { \
+	{k0A, k0B, k0C, k0D, k0E, k0F}, \
+	{k1A, k1B, k1C, k1D, k1E, k1F}, \
+	{k2A, k2B, k2C, k2D, k2E, k2F}, \
+	{k3A, k3B, k3C, k3D, k3E, k3F}, \
+	{k4A, k4B, k4C, k4D, k4E, k4F} \
+}
